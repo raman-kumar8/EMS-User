83c45887400cab5795938b3aeb425b75bbfd3e48
raman-kumar8 <raman.kumar@wissen.com>
2025-05-11 20:49:56 +0530

    intial structure

---
+++ src/main/java/com/example/emsuser/EmsUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.emsuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsUserApplication.class, args);
+    }
+
+}
