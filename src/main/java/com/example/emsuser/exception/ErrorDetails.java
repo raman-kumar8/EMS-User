0bdc844e73296a7a72469322751856c6e010c639
Anshikamidha <anshikamidha125@gmail.com>
2025-05-29 13:56:54 +0530

    Validations added

--- src/main/java/com/example/emsuser/exception/ErrorDetails.java
+++ src/main/java/com/example/emsuser/exception/ErrorDetails.java
@@ -3,10 +3,10 @@ package com.example.emsuser.exception;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
-@AllArgsConstructor
-public class ErrorDetails {
+    @Data
+    @AllArgsConstructor
+    public class ErrorDetails {
 
-    private String message;
-    private int status;
-}
+        private String message;
+        private int status;
+    }
